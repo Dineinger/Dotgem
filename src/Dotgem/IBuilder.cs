ce3b01a36f14752cc0fa2587356cd3c480c76721
Dineinger <45884614+Dineinger@users.noreply.github.com>
2022-06-18 01:08:38 +0200

    [add] .editorconfig [add] IBuilder

---
+++ src/Dotgem/IBuilder.cs
@@ -0,0 +1,14 @@
+﻿namespace Dotgem;
+
+/// <summary>
+/// Defines a builder class.
+/// </summary>
+/// <typeparam name="TProduct">The object type builded by the builder.</typeparam>
+public interface IBuilder<TProduct>
+{
+    /// <summary>
+    /// Building the object.
+    /// </summary>
+    /// <returns>The builded object.</returns>
+    TProduct Build();
+}
