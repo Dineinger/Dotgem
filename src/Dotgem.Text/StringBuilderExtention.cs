21517d609623f3714f806f8a2bbd021da88bf4f6
Dineinger <45884614+Dineinger@users.noreply.github.com>
2022-06-18 16:29:15 +0200

    [add] StringBuilderExtention

---
+++ src/Dotgem.Text/StringBuilderExtention.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Dotgem.Text;
+
+/// <summary>
+/// Extent the <see cref="StringBuilder"/>
+/// </summary>
+public static class StringBuilderExtention
+{
+    /// <summary>
+    /// Append spaces to the <see cref="StringBuilder" />.
+    /// </summary>
+    /// <param name="builder">The <see cref="StringBuilder" />.</param>
+    /// <param name="spaceCount">The amount of spaces.</param>
+    /// <returns>The same <see cref="StringBuilder" />. Use it as a Fluent API.</returns>
+    public static StringBuilder AppenSpace(this StringBuilder builder, int spaceCount)
+    {
+        for (int a = 0; a < spaceCount; a++)
+        {
+            builder.Append(' ');
+        }
+        return builder;
+    }
+}
