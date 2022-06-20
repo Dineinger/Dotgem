6b6b55f531c33ff5ae980c90f0650e009cc69b74
Dineinger <45884614+Dineinger@users.noreply.github.com>
2022-06-20 16:29:36 +0200

    [add] IsBetweenExtentions

---
+++ src/Dotgem.Text/IsBetweenExtentions.cs
@@ -0,0 +1,40 @@
+﻿namespace Dotgem.Text;
+
+public static class IsBetweenExtentions
+{
+    /// <summary>
+    /// Determins if a char is between 2 other chars.
+    /// </summary>
+    /// <param name="self">The char which is checked.</param>
+    /// <param name="from">The smaller value.</param>
+    /// <param name="to">The bigger value.</param>
+    /// <returns>true if the value is in between, otherwise false.</returns>
+    public static bool IsBetween(this char self, char from, char to)
+    {
+        return self >= from && self <= to;
+    }
+
+    /// <summary>
+    /// Determins if a char is between 2 integers.
+    /// </summary>
+    /// <param name="self">The char which is checked.</param>
+    /// <param name="from">The smaller value.</param>
+    /// <param name="to">The bigger value.</param>
+    /// <returns>true if the value is in between, otherwise false.</returns>
+    public static bool IsBetween(this char self, int from, int to)
+    {
+        return self >= from && self <= to;
+    }
+
+    /// <summary>
+    /// Determins if a integer is between 2 other integers.
+    /// </summary>
+    /// <param name="self">The int which is checked.</param>
+    /// <param name="from">The smaller value.</param>
+    /// <param name="to">The bigger value.</param>
+    /// <returns>true if the value is in between, otherwise false.</returns>
+    public static bool IsBetween(this int self, int from, int to)
+    {
+        return self >= from && self <= to;
+    }
+}
