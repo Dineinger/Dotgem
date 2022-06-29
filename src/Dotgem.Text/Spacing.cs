0e8def6b7e1eb6584756584004355119d347fafb
Dineinger <45884614+Dineinger@users.noreply.github.com>
2022-06-29 12:21:43 +0200

    [add] Spacing
    Spacing combines diffrent classes for counting, creating and manipulating space

---
+++ src/Dotgem.Text/Spacing.cs
@@ -0,0 +1,21 @@
+﻿namespace Dotgem.Text;
+
+public class Spacing
+{
+    private IFiller? _filler;
+    public IFiller Filler => _filler ??= new Filler();
+
+    private ICharCounter? _charCounter;
+    public ICharCounter CharCounter => _charCounter ??= new CharCounter();
+
+    public Spacing()
+    {
+
+    }
+
+    public Spacing(IFiller filler, ICharCounter charCounter)
+    {
+        _filler = filler;
+        _charCounter = charCounter;
+    }
+}
